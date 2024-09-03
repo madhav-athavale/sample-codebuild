8b64bef3d7acb0bfddb0163cebc69ade7956205b
madhav.athavale@gmail.com <madhav.athavale@gmail.com>
2024-09-02 23:50:29 -0400

    New Print

--- lambda_function.py
+++ lambda_function.py
@@ -8,6 +8,7 @@ def lambda_handler(event, context):
     api_url = f"https://api.openweathermap.org/data/2.5/weather?lat=40.71427&lon=-74.00597&appid=b5cd2f38a8c37fc39dfc85ce74c1af5c"
     response = requests.get(api_url)
     json_object = json.loads(response.text)
+    print("Hello")
     print(json_object)
     return json_object
 
